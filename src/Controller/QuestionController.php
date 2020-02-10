0ab1054c1bd5f25ff1037cb6391cfb8f16b49deb
Ryan Weaver <ryan@thatsquality.com>
2020-02-10 12:24:19 -0500

    tuts hero working...

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class QuestionController
 {
+    /**
+     * @Route()
+     */
     public function homepage()
     {
         return new Response('OMG! My first page already! WOOO!');
