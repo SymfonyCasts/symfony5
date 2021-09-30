94526a2829375e8417fbacd2c330291c1684a747
Ryan Weaver <ryan@thatsquality.com>
2021-09-30 15:25:59 -0400

    [tuts-apply-diff] Applying existing diff file for step foundry-faker-boolean

--- src/Factory/QuestionFactory.php
+++ src/Factory/QuestionFactory.php
@@ -36,7 +36,7 @@ opening the front door and walking out! I've been out all afternoon
 Does anyone have a spell to call your pants back?
 EOF
             ,
-            'askedAt' => rand(1, 10) > 2 ? self::faker()->dateTimeBetween('-100 days', '-1 minute') : null,
+            'askedAt' => self::faker()->boolean(70) ? self::faker()->dateTimeBetween('-100 days', '-1 minute') : null,
             'votes' => rand(-20, 50),
         ];
     }
