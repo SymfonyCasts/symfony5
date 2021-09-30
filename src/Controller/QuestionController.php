3a816e67a781f6c53b9d2a1114ed028ce3912250
Ryan Weaver <ryan@thatsquality.com>
2021-09-30 15:25:55 -0400

    [tuts-apply-diff] Applying existing diff file for step vote-action-dump-post-vars

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -8,6 +8,7 @@ use App\Service\MarkdownHelper;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -96,8 +97,8 @@ EOF
     /**
      * @Route("/questions/{slug}/vote", name="app_question_vote", methods="POST")
      */
-    public function questionVote(Question $question)
+    public function questionVote(Question $question, Request $request)
     {
-        dd($question);
+        dd($question, $request->request->all());
     }
 }
