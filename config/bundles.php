8edc4817f82743c4d65fa31857f78c8e04844d14
Ryan Weaver <ryan@thatsquality.com>
2021-09-30 15:26:01 -0400

    [tuts-apply-diff] Applying existing diff file for step behaviors-install-stof-extensions

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
 ];
