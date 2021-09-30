4731039142d3dfa0cc78076c5bb4c760f6e0d1b1
Ryan Weaver <ryan@thatsquality.com>
2021-09-30 15:25:51 -0400

    [tuts-apply-diff] Applying existing diff file for step more-queries-reuse-query

--- src/Repository/QuestionRepository.php
+++ src/Repository/QuestionRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Question;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -24,12 +25,18 @@ class QuestionRepository extends ServiceEntityRepository
       */
     public function findAllAskedOrderedByNewest()
     {
-        return $this->createQueryBuilder('q')
-            ->andWhere('q.askedAt IS NOT NULL')
+        $qb = $this->createQueryBuilder('q');
+
+        return $this->addIsAskedQueryBuilder($qb)
             ->orderBy('q.askedAt', 'DESC')
             ->getQuery()
             ->getResult()
         ;
+    }
+
+    private function addIsAskedQueryBuilder(QueryBuilder $qb): QueryBuilder
+    {
+        return $qb->andWhere('q.askedAt IS NOT NULL');
     }
 
     /*
