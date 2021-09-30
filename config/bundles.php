a6d68fe4b677175cef44891f9a5a386c2a61ea71
Ryan Weaver <ryan@thatsquality.com>
2021-09-30 15:25:33 -0400

    [tuts-apply-diff] Applying existing diff file for step maker-install-it

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Knp\Bundle\MarkdownBundle\KnpMarkdownBundle::class => ['all' => true],
     Sentry\SentryBundle\SentryBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
