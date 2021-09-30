76910e752be407478fb943d38abcda9e1bb44c73
Ryan Weaver <ryan@thatsquality.com>
2021-09-30 15:25:47 -0400

    [tuts-apply-diff] Applying existing diff file for step ago-install-knptimebundle

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
 ];
