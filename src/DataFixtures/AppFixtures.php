7f910a16079563d9edcb0bffd1f39db109022576
Ryan Weaver <ryan@thatsquality.com>
2021-09-30 15:26:11 -0400

    [tuts-apply-diff] Applying existing diff file for step foundryrelation-use-factory

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Answer;
 use App\Entity\Question;
+use App\Factory\AnswerFactory;
 use App\Factory\QuestionFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -20,18 +21,7 @@ class AppFixtures extends Fixture
             ->create()
         ;
 
-        $answer = new Answer();
-        $answer->setContent('This question is the best? I wish... I knew the answer.');
-        $answer->setUsername('weaverryan');
-
-        $question = new Question();
-        $question->setName('How to un-disappear your wallet.');
-        $question->setQuestion('... I should not have done this...');
-
-        $answer->setQuestion($question);
-
-        $manager->persist($answer);
-        $manager->persist($question);
+        AnswerFactory::createMany(100);
 
         $manager->flush();
     }
