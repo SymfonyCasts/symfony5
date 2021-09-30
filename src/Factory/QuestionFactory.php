49c90b0df672e4474bf66e96f96e191e1575c432
Ryan Weaver <ryan@thatsquality.com>
2021-09-30 15:25:58 -0400

    [tuts-apply-diff] Applying existing diff file for step foundry-make-factory-question

---
+++ src/Factory/QuestionFactory.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Question;
+use App\Repository\QuestionRepository;
+use Zenstruck\Foundry\RepositoryProxy;
+use Zenstruck\Foundry\ModelFactory;
+use Zenstruck\Foundry\Proxy;
+
+/**
+ * @method static Question|Proxy findOrCreate(array $attributes)
+ * @method static Question|Proxy random()
+ * @method static Question[]|Proxy[] randomSet(int $number)
+ * @method static Question[]|Proxy[] randomRange(int $min, int $max)
+ * @method static QuestionRepository|RepositoryProxy repository()
+ * @method Question|Proxy create($attributes = [])
+ * @method Question[]|Proxy[] createMany(int $number, $attributes = [])
+ */
+final class QuestionFactory extends ModelFactory
+{
+    protected function getDefaults(): array
+    {
+        return [
+            // TODO add your default values here (https://github.com/zenstruck/foundry#model-factories)
+        ];
+    }
+
+    protected function initialize(): self
+    {
+        // see https://github.com/zenstruck/foundry#initialization
+        return $this
+            // ->beforeInstantiate(function(Question $question) {})
+        ;
+    }
+
+    protected static function getClass(): string
+    {
+        return Question::class;
+    }
+}
