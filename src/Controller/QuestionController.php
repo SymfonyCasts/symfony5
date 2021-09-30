c92ee5441987e24c6f4ebbdd332de50a906087e8
Ryan Weaver <ryan@thatsquality.com>
2021-09-30 15:25:19 -0400

    [tuts-apply-diff] Applying existing diff file for step config-dump-cache

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -42,7 +42,7 @@ class QuestionController extends AbstractController
             return $markdownParser->transformMarkdown($questionText);
         });
 
-        dd($markdownParser);
+        dump($cache);
 
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
