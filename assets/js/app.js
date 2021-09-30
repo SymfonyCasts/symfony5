67be0d374754b82b4cb591233fc26e1da14a75ed
Ryan Weaver <ryan@thatsquality.com>
2021-09-30 15:25:38 -0400

    [tuts-apply-diff] Applying existing diff file for step pre-ep3-re-work-upvotes-styling

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,12 +15,12 @@ import $ from 'jquery';
  * Simple (ugly) code to handle the comment vote up/down
  */
 var $container = $('.js-vote-arrows');
-$container.find('a').on('click', function(e) {
+$container.find('button').on('click', function(e) {
     e.preventDefault();
-    var $link = $(e.currentTarget);
+    var $button = $(e.currentTarget);
 
     $.ajax({
-        url: '/comments/10/vote/'+$link.data('direction'),
+        url: '/comments/10/vote/'+$button.val(),
         method: 'POST'
     }).then(function(data) {
         $container.find('.js-vote-total').text(data.votes);
