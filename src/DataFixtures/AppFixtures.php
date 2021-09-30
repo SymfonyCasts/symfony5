d5738b6c0c27df9d167d751606c30f3e588f0892
Ryan Weaver <ryan@thatsquality.com>
2021-09-30 15:25:59 -0400

    [tuts-apply-diff] Applying existing diff file for step foundry-createmany

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -11,7 +11,7 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        QuestionFactory::new()->create();
+        QuestionFactory::new()->createMany(20);
 
         $manager->flush();
     }
