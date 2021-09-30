9e188f6df60535f4d705040b15b5de5166bc794f
Ryan Weaver <ryan@thatsquality.com>
2021-09-30 15:25:16 -0400

    [tuts-apply-diff] Applying existing diff file for step install-bundle-add-markdown-to-text

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -34,7 +34,7 @@ class QuestionController extends AbstractController
             'Honestly, I like furry shoes better than MY cat',
             'Maybe... try saying the spell backwards?',
         ];
-        $questionText = 'I\'ve been turned into a cat, any thoughts on how to turn back? While I\'m adorable, I don\'t really care for cat food.';
+        $questionText = 'I\'ve been turned into a cat, any thoughts on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
 
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
