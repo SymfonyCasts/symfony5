fa6179fd44913c80409043ce853409da5d1ef02c
Ryan Weaver <ryan@thatsquality.com>
2021-09-30 15:26:03 -0400

    [tuts-apply-diff] Applying existing diff file for step behaviors-add-timestampableentity

--- src/Entity/Question.php
+++ src/Entity/Question.php
@@ -5,12 +5,15 @@ namespace App\Entity;
 use App\Repository\QuestionRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
 
 /**
  * @ORM\Entity(repositoryClass=QuestionRepository::class)
  */
 class Question
 {
+    use TimestampableEntity;
+
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
