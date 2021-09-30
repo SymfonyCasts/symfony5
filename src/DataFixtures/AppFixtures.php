e77744d70b50df518b7c0a9a4bc6d86fd8f33cc7
Ryan Weaver <ryan@thatsquality.com>
2021-09-30 15:26:28 -0400

    [tuts-apply-diff] Applying existing diff file for step complexfixtures-also-set-tag

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -20,7 +20,9 @@ class AppFixtures extends Fixture
 
         $questions = QuestionFactory::createMany(20, function() {
             return [
-                'questionTags' => QuestionTagFactory::new()->many(1, 5),
+                'questionTags' => QuestionTagFactory::new([
+                    'tag' => TagFactory::random()
+                ])->many(1, 5),
             ];
         });
 
