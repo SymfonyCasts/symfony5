e581c2dd1aad66ac24d67138b1a6bca7c038b1d2
Ryan Weaver <ryan@thatsquality.com>
2021-09-30 15:26:25 -0400

    [tuts-apply-diff] Applying existing diff file for step complexmanytomany-generate-questiontag

--- src/Entity/Question.php
+++ src/Entity/Question.php
@@ -57,9 +57,15 @@ class Question
      */
     private $answers;
 
+    /**
+     * @ORM\OneToMany(targetEntity=QuestionTag::class, mappedBy="question")
+     */
+    private $questionTags;
+
     public function __construct()
     {
         $this->answers = new ArrayCollection();
+        $this->questionTags = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -177,6 +183,36 @@ class Question
             // set the owning side to null (unless already changed)
             if ($answer->getQuestion() === $this) {
                 $answer->setQuestion(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|QuestionTag[]
+     */
+    public function getQuestionTags(): Collection
+    {
+        return $this->questionTags;
+    }
+
+    public function addQuestionTag(QuestionTag $questionTag): self
+    {
+        if (!$this->questionTags->contains($questionTag)) {
+            $this->questionTags[] = $questionTag;
+            $questionTag->setQuestion($this);
+        }
+
+        return $this;
+    }
+
+    public function removeQuestionTag(QuestionTag $questionTag): self
+    {
+        if ($this->questionTags->removeElement($questionTag)) {
+            // set the owning side to null (unless already changed)
+            if ($questionTag->getQuestion() === $this) {
+                $questionTag->setQuestion(null);
             }
         }
 
