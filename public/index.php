3435e060bd20543dce5d54a5e3bc158fd716524c
Ryan Weaver <ryan@thatsquality.com>
2021-09-30 15:25:41 -0400

    [tuts-apply-diff] Applying existing diff file for step docker-dd-server

--- public/index.php
+++ public/index.php
@@ -7,6 +7,8 @@ use Symfony\Component\HttpFoundation\Request;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
+dd($_SERVER);
+
 (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 
 if ($_SERVER['APP_DEBUG']) {
