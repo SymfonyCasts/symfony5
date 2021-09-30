35679c3d2c8959d10e5cc95120d2335957a7b26c
Ryan Weaver <ryan@thatsquality.com>
2021-09-30 15:24:58 -0400

    [tuts-apply-diff] Applying existing diff file for step pages-convert-route-to-annotation

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class QuestionController
 {
+    /**
+     * @Route("/")
+     */
     public function homepage()
     {
         return new Response('What a bewitching controller we have conjured!');
