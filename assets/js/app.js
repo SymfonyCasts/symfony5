79d776039164b3fedf97285a9bd7098aa7637413
Ryan Weaver <ryan@thatsquality.com>
2021-09-30 15:25:14 -0400

    [tuts-apply-diff] Applying existing diff file for step encore-import-jquery

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,7 +9,7 @@
 import '../css/app.css';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
+import $ from 'jquery';
 
 /**
  * Simple (ugly) code to handle the comment vote up/down
