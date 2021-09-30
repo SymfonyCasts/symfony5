cc5cac80a3830b0890ab21af884fd156cb2aaedb
Ryan Weaver <ryan@thatsquality.com>
2021-09-30 15:26:03 -0400

    [tuts-apply-diff] Applying existing diff file for step timestampable-fix-migration

--- migrations/Version20200709153558.php
+++ migrations/Version20200709153558.php
@@ -20,7 +20,8 @@ final class Version20200709153558 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE question ADD created_at DATETIME NOT NULL, ADD updated_at DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE question ADD created_at DATETIME DEFAULT NULL, ADD updated_at DATETIME DEFAULT NULL');
+        $this->addSql('UPDATE question SET created_at = NOW(), updated_at = NOW()');
     }
 
     public function down(Schema $schema) : void
