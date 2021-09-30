694d44fc1c79d7a01dabcbdafe48f913f1ce5df8
Ryan Weaver <ryan@thatsquality.com>
2021-09-30 15:25:23 -0400

    [tuts-apply-diff] Applying existing diff file for step parameters-set-cache-adapter-parameter

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -30,6 +30,8 @@ class QuestionController extends AbstractController
      */
     public function show($slug, MarkdownHelper $markdownHelper)
     {
+        dump($this->getParameter('cache_adapter'));
+
         $answers = [
             'Make sure your cat is sitting `purrrfectly` still 🤣',
             'Honestly, I like furry shoes better than MY cat',
