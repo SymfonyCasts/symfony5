b0ff80327f423a19bffd29bcf869b33cb8228500
Ryan Weaver <ryan@thatsquality.com>
2021-09-30 15:26:30 -0400

    [tuts-apply-diff] Applying existing diff file for step pagerfanta-install-it

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
 ];
