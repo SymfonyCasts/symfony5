445bdac6eb2997990b331ca5526e9ac0c5b6b4ad
Ryan Weaver <ryan@thatsquality.com>
2021-09-30 15:25:50 -0400

    [tuts-apply-diff] Applying existing diff file for step more-queries-autowire-repository

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Question;
+use App\Repository\QuestionRepository;
 use App\Service\MarkdownHelper;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
@@ -25,9 +26,8 @@ class QuestionController extends AbstractController
     /**
      * @Route("/", name="app_homepage")
      */
-    public function homepage(EntityManagerInterface $entityManager)
+    public function homepage(QuestionRepository $repository)
     {
-        $repository = $entityManager->getRepository(Question::class);
         $questions = $repository->findAllAskedOrderedByNewest();
 
         return $this->render('question/homepage.html.twig', [
