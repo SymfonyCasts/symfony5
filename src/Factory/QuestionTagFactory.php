d190f651ff3a1204618768caf51ea2d4fd19c30d
Ryan Weaver <ryan@thatsquality.com>
2021-09-30 15:26:27 -0400

    [tuts-apply-diff] Applying existing diff file for step complexfixtures-getdefaults

--- src/Factory/QuestionTagFactory.php
+++ src/Factory/QuestionTagFactory.php
@@ -38,7 +38,8 @@ final class QuestionTagFactory extends ModelFactory
     protected function getDefaults(): array
     {
         return [
-            // TODO add your default values here (https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories)
+            'question' => QuestionFactory::new(),
+            'tag' => TagFactory::new(),
             'taggedAt' => self::faker()->datetime(),
         ];
     }
