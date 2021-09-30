5fc27f3b8359fa73563687372f6225ac76910161
Ryan Weaver <ryan@thatsquality.com>
2021-09-30 15:25:50 -0400

    [tuts-apply-diff] Applying existing diff file for step more-queries-write-query-builder

--- src/Repository/QuestionRepository.php
+++ src/Repository/QuestionRepository.php
@@ -25,10 +25,8 @@ class QuestionRepository extends ServiceEntityRepository
     public function findAllAskedOrderedByNewest()
     {
         return $this->createQueryBuilder('q')
-            ->andWhere('q.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('q.id', 'ASC')
-            ->setMaxResults(10)
+            ->andWhere('q.askedAt IS NOT NULL')
+            ->orderBy('q.askedAt', 'DESC')
             ->getQuery()
             ->getResult()
         ;
