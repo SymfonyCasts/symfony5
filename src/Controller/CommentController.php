20321f24fa5d00315f7b644c6d1fe6162848dd8e
Ryan Weaver <ryan@thatsquality.com>
2021-09-30 15:25:08 -0400

    [tuts-apply-diff] Applying existing diff file for step ajax-implement-endpoint

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -3,7 +3,25 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
 
 class CommentController extends AbstractController
 {
+    /**
+     * @Route("/comments/{id}/vote/{direction}")
+     */
+    public function commentVote($id, $direction)
+    {
+        // todo - use id to query the database
+
+        // use real logic here to save this to the database
+        if ($direction === 'up') {
+            $currentVoteCount = rand(7, 100);
+        } else {
+            $currentVoteCount = rand(0, 5);
+        }
+
+        return new JsonResponse(['votes' => $currentVoteCount]);
+    }
 }
