b058fdff1d0b02884510606b2d96b54ced356b50
Ryan Weaver <ryan@thatsquality.com>
2021-09-30 15:26:24 -0400

    [tuts-apply-diff] Applying existing diff file for step tagfixtures-getdefaults

--- src/Factory/TagFactory.php
+++ src/Factory/TagFactory.php
@@ -38,10 +38,8 @@ final class TagFactory extends ModelFactory
     protected function getDefaults(): array
     {
         return [
-            // TODO add your default values here (https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories)
-            'name' => self::faker()->text(),
-            'createdAt' => null, // TODO add DATETIME ORM type manually
-            'updatedAt' => null, // TODO add DATETIME ORM type manually
+            'name' => self::faker()->word(),
+            'createdAt' => self::faker()->dateTimeBetween('-1 year'),
         ];
     }
 
