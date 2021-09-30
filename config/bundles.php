d248c6db8c4b127c21df93b602906eb28c1ff532
Ryan Weaver <ryan@thatsquality.com>
2021-09-30 15:25:30 -0400

    [tuts-apply-diff] Applying existing diff file for step env-vars-install-sentry

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Knp\Bundle\MarkdownBundle\KnpMarkdownBundle::class => ['all' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['all' => true],
 ];
