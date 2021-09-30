23d4e692f9e4d1d8a894ff3c8fcd1161335f024d
Ryan Weaver <ryan@thatsquality.com>
2021-09-30 15:26:25 -0400

    [tuts-apply-diff] Applying existing diff file for step complexmanytomany-remove-question-tags-relation

--- src/Entity/Tag.php
+++ src/Entity/Tag.php
@@ -27,16 +27,6 @@ class Tag
      */
     private $name;
 
-    /**
-     * @ORM\ManyToMany(targetEntity=Question::class, mappedBy="tags")
-     */
-    private $questions;
-
-    public function __construct()
-    {
-        $this->questions = new ArrayCollection();
-    }
-
     public function getId(): ?int
     {
         return $this->id;
@@ -50,33 +40,6 @@ class Tag
     public function setName(string $name): self
     {
         $this->name = $name;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|Question[]
-     */
-    public function getQuestions(): Collection
-    {
-        return $this->questions;
-    }
-
-    public function addQuestion(Question $question): self
-    {
-        if (!$this->questions->contains($question)) {
-            $this->questions[] = $question;
-            $question->addTag($this);
-        }
-
-        return $this;
-    }
-
-    public function removeQuestion(Question $question): self
-    {
-        if ($this->questions->removeElement($question)) {
-            $question->removeTag($this);
-        }
 
         return $this;
     }
