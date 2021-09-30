a53795830ef17cacdad6c9697a8bc23faf47901d
Ryan Weaver <ryan@thatsquality.com>
2021-09-30 15:25:57 -0400

    [tuts-apply-diff] Applying existing diff file for step fixtures-move-new-question-code

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -39,38 +39,9 @@ class QuestionController extends AbstractController
     /**
      * @Route("/questions/new")
      */
-    public function new(EntityManagerInterface $entityManager)
+    public function new()
     {
-        $question = new Question();
-        $question->setName('Missing pants')
-            ->setSlug('missing-pants-'.rand(0, 1000))
-            ->setQuestion(<<<EOF
-Hi! So... I'm having a *weird* day. Yesterday, I cast a spell
-to make my dishes wash themselves. But while I was casting it,
-I slipped a little and I think `I also hit my pants with the spell`.
-
-When I woke up this morning, I caught a quick glimpse of my pants
-opening the front door and walking out! I've been out all afternoon
-(with no pants mind you) searching for them.
-
-Does anyone have a spell to call your pants back?
-EOF
-            );
-
-        if (rand(1, 10) > 2) {
-            $question->setAskedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
-        }
-
-        $question->setVotes(rand(-20, 50));
-
-        $entityManager->persist($question);
-        $entityManager->flush();
-
-        return new Response(sprintf(
-            'Well hallo! The shiny new question is id #%d, slug: %s',
-            $question->getId(),
-            $question->getSlug()
-        ));
+        return new Response('Sounds like a GREAT feature for V2!');
     }
 
     /**
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Question;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,29 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $question = new Question();
+        $question->setName('Missing pants')
+            ->setSlug('missing-pants-'.rand(0, 1000))
+            ->setQuestion(<<<EOF
+Hi! So... I'm having a *weird* day. Yesterday, I cast a spell
+to make my dishes wash themselves. But while I was casting it,
+I slipped a little and I think `I also hit my pants with the spell`.
+
+When I woke up this morning, I caught a quick glimpse of my pants
+opening the front door and walking out! I've been out all afternoon
+(with no pants mind you) searching for them.
+
+Does anyone have a spell to call your pants back?
+EOF
+            );
+
+        if (rand(1, 10) > 2) {
+            $question->setAskedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
+        }
+
+        $question->setVotes(rand(-20, 50));
+
+        $entityManager->persist($question);
 
         $manager->flush();
     }
