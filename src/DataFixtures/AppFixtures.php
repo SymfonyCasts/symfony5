d8a86558e8e2f8bdce741bbfa5a68fc1b5439ca8
Ryan Weaver <ryan@thatsquality.com>
2021-09-30 15:25:58 -0400

    [tuts-apply-diff] Applying existing diff file for step foundry-move-over-to-defaults

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Question;
+use App\Factory\QuestionFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -10,29 +11,7 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $question = new Question();
-        $question->setName('Missing pants')
-            ->setSlug('missing-pants-'.rand(0, 1000))
-            ->setQuestion(<<<EOF
-Hi! So... I'm having a *weird* day. Yesterday, I cast a spell
-to make my dishes wash themselves. But while I was casting it,
-I slipped a little and I think `I also hit my pants with the spell`.
-
-When I woke up this morning, I caught a quick glimpse of my pants
-opening the front door and walking out! I've been out all afternoon
-(with no pants mind you) searching for them.
-
-Does anyone have a spell to call your pants back?
-EOF
-            );
-
-        if (rand(1, 10) > 2) {
-            $question->setAskedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
-        }
-
-        $question->setVotes(rand(-20, 50));
-
-        $manager->persist($question);
+        QuestionFactory::new()->create();
 
         $manager->flush();
     }
--- src/Factory/QuestionFactory.php
+++ src/Factory/QuestionFactory.php
@@ -22,7 +22,22 @@ final class QuestionFactory extends ModelFactory
     protected function getDefaults(): array
     {
         return [
-            // TODO add your default values here (https://github.com/zenstruck/foundry#model-factories)
+            'name' => 'Missing pants',
+            'slug' => 'missing-pants-'.rand(0, 1000),
+            'question' => <<<EOF
+Hi! So... I'm having a *weird* day. Yesterday, I cast a spell
+to make my dishes wash themselves. But while I was casting it,
+I slipped a little and I think `I also hit my pants with the spell`.
+
+When I woke up this morning, I caught a quick glimpse of my pants
+opening the front door and walking out! I've been out all afternoon
+(with no pants mind you) searching for them.
+
+Does anyone have a spell to call your pants back?
+EOF
+            ,
+            'askedAt' => rand(1, 10) > 2 ? new \DateTime(sprintf('-%d days', rand(1, 100))) : null,
+            'votes' => rand(-20, 50),
         ];
     }
 
