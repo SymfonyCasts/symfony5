da24eb9bea8cff6f5f4dbe14360b89eeadc99f77
Ryan Weaver <ryan@thatsquality.com>
2021-09-30 15:25:40 -0400

    [tuts-apply-diff] Applying existing diff file for step doctrine-install-install-doctrine

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,6 @@ return [
     Knp\Bundle\MarkdownBundle\KnpMarkdownBundle::class => ['all' => true],
     Sentry\SentryBundle\SentryBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
