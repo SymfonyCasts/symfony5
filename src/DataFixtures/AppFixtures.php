4679f5528d6ce6dfbbb0289790167702f7b6f44f
Ryan Weaver <ryan@thatsquality.com>
2021-09-30 15:26:15 -0400

    [tuts-apply-diff] Applying existing diff file for step inverserelation-remove-appfixture-code

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -27,20 +27,6 @@ class AppFixtures extends Fixture
             ];
         });
 
-        $question = QuestionFactory::createOne();
-        $answer1 = new Answer();
-        $answer1->setContent('answer 1');
-        $answer1->setUsername('weaverryan');
-        $answer2 = new Answer();
-        $answer2->setContent('answer 1');
-        $answer2->setUsername('weaverryan');
-
-        $question->addAnswer($answer1);
-        $question->addAnswer($answer2);
-
-        $manager->persist($answer1);
-        $manager->persist($answer2);
-
         $manager->flush();
     }
 }
