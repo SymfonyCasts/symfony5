3d464c0879f69c1f887b04dbed137ee4df3ff7fa
bocharsky-bw <bocharsky.bw@gmail.com>
2022-01-24 20:50:37 +0200

    [tuts-apply-diff] Applying existing diff file for step twig-render-template

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
     /**
      * @Route("/")
@@ -20,9 +21,8 @@ class QuestionController
      */
     public function show($slug)
     {
-        return new Response(sprintf(
-            'Future page to show the question "%s"!',
-            ucwords(str_replace('-', ' ', $slug))
-        ));
+        return $this->render('question/show.html.twig', [
+            'question' => ucwords(str_replace('-', ' ', $slug))
+        ]);
     }
 }
