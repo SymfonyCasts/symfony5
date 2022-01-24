7127702398c50830043877183306b9c536979bcd
bocharsky-bw <bocharsky.bw@gmail.com>
2022-01-24 20:50:44 +0200

    [tuts-apply-diff] Applying existing diff file for step assets-render-homepage-template

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -13,7 +13,7 @@ class QuestionController extends AbstractController
      */
     public function homepage()
     {
-        return new Response('What a bewitching controller we have conjured!');
+        return $this->render('question/homepage.html.twig');
     }
 
     /**
