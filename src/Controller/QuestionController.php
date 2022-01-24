57c930b6c93ec3077b76236459a49db6a38289f3
bocharsky-bw <bocharsky.bw@gmail.com>
2022-01-24 20:50:40 +0200

    [tuts-apply-diff] Applying existing diff file for step profiler-use-dump

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -27,6 +27,8 @@ class QuestionController extends AbstractController
             'Maybe... try saying the spell backwards?',
         ];
 
+        dump($slug, $this);
+
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
             'answers' => $answers,
