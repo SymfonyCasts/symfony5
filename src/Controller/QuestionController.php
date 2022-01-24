5438d68bfca408453ecfbca7b8e51b57c0979b0a
bocharsky-bw <bocharsky.bw@gmail.com>
2022-01-24 20:50:48 +0200

    [tuts-apply-diff] Applying existing diff file for step services-render-via-twig

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -5,15 +5,21 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class QuestionController extends AbstractController
 {
     /**
      * @Route("/", name="app_homepage")
      */
-    public function homepage()
+    public function homepage(Environment $twigEnvironment)
     {
-        return $this->render('question/homepage.html.twig');
+        // fun example of using the Twig service directly!
+        $html = $twigEnvironment->render('question/homepage.html.twig');
+
+        return new Response($html);
+
+        //return $this->render('question/homepage.html.twig');
     }
 
     /**
