fa5905f077f9292c6a0ebc2118d8e06363f1489e
bocharsky-bw <bocharsky.bw@gmail.com>
2022-01-24 20:51:46 +0200

    [tuts-apply-diff] Applying existing diff file for step install-bundle-autowire-service

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -27,7 +28,7 @@ class QuestionController extends AbstractController
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug)
+    public function show($slug, MarkdownParserInterface $markdownParser)
     {
         $answers = [
             'Make sure your cat is sitting purrrfectly still 🤣',
@@ -35,10 +36,11 @@ class QuestionController extends AbstractController
             'Maybe... try saying the spell backwards?',
         ];
         $questionText = 'I\'ve been turned into a cat, any thoughts on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
+        $parsedQuestionText = $markdownParser->transformMarkdown($questionText);
 
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
-            'questionText' => $questionText,
+            'questionText' => $parsedQuestionText,
             'answers' => $answers,
         ]);
     }
