34001d9e08b20b229c813901f5ce421dd80f63d2
bocharsky-bw <bocharsky.bw@gmail.com>
2022-01-24 20:50:48 +0200

    [tuts-apply-diff] Applying existing diff file for step services-autowire-logger

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,14 +12,16 @@ class CommentController extends AbstractController
     /**
      * @Route("/comments/{id}/vote/{direction<up|down>}", methods="POST")
      */
-    public function commentVote($id, $direction)
+    public function commentVote($id, $direction, LoggerInterface $logger)
     {
         // todo - use id to query the database
 
         // use real logic here to save this to the database
         if ($direction === 'up') {
+            $logger->info('Voting up!');
             $currentVoteCount = rand(7, 100);
         } else {
+            $logger->info('Voting down!');
             $currentVoteCount = rand(0, 5);
         }
 
