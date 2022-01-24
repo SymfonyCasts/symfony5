7fb7105d36230185aa954ef98397da471bbf8dee
bocharsky-bw <bocharsky.bw@gmail.com>
2022-01-24 20:52:06 +0200

    [tuts-apply-diff] Applying existing diff file for step maker-safe-for-html

--- src/Twig/MarkdownExtension.php
+++ src/Twig/MarkdownExtension.php
@@ -22,7 +22,7 @@ class MarkdownExtension extends AbstractExtension
             // If your filter generates SAFE HTML, you should add a third
             // parameter: ['is_safe' => ['html']]
             // Reference: https://twig.symfony.com/doc/2.x/advanced.html#automatic-escaping
-            new TwigFilter('parse_markdown', [$this, 'parseMarkdown']),
+            new TwigFilter('parse_markdown', [$this, 'parseMarkdown'], ['is_safe' => ['html']]),
         ];
     }
 
