fbe103e28a5a32e686a4d58ac59516127797e50a
bocharsky-bw <bocharsky.bw@gmail.com>
2022-01-27 21:20:35 +0200

    Install Doctrine ORM pack

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Sentry\SentryBundle\SentryBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
 ];
