b2786ef56fd1a1645f3ee79ebfaa218d592986cb
bocharsky-bw <bocharsky.bw@gmail.com>
2022-02-08 13:51:13 +0200

    Addd missing Composer deps

--- config/bundles.php
+++ config/bundles.php
@@ -20,4 +20,6 @@ return [
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
