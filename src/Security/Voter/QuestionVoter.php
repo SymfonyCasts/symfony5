b8f2aa7db98317961cd186907939ed0b6eef357a
bocharsky-bw <bocharsky.bw@gmail.com>
2022-02-16 15:00:45 +0200

    Port voter as well

---
+++ src/Security/Voter/QuestionVoter.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\Question;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class QuestionVoter extends Voter
+{
+    private Security $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    protected function supports(string $attribute, $subject): bool
+    {
+        // https://symfony.com/doc/current/security/voters.html
+        return in_array($attribute, ['EDIT'])
+            && $subject instanceof \App\Entity\Question;
+    }
+
+    protected function voteOnAttribute(string $attribute, $subject, TokenInterface $token): bool
+    {
+        /** @var User $user */
+        $user = $token->getUser();
+        // if the user is anonymous, do not grant access
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        if (!$subject instanceof Question) {
+            throw new \Exception('Wrong type somehow passed');
+        }
+
+        if ($this->security->isGranted('ROLE_ADMIN')) {
+            return true;
+        }
+
+        // ... (check conditions and return true to grant permission) ...
+        switch ($attribute) {
+            case 'EDIT':
+                return $user === $subject->getOwner();
+        }
+
+        return false;
+    }
+}
